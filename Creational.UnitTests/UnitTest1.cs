edc646b41ef3cb1179b11233a65de7e2dfac2487
Sebastian Castro <sebastian.castro@ukg.com>
2022-06-26 23:06:14 -0400

    Reorganizing FileStructure

---
+++ Creational.UnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+
+namespace Creational.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            //here we can setup the singleton object for testing
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
